b12de29485c2a35a89c4ac5d0af42d50dc7f6490
Andy <andrewpeliza@hotmail.com>
2018-06-05 15:07:57 +0100

    Auth signup started using Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCeTUgxDnLYfDnj2wpXU9re_-A3Vy8QpE4',
+      authDomain: 'ng-recipe-book-9f435.firebaseapp.com'
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
