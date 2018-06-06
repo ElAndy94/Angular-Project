6e3212076cc6c5ffb9997450656810176984dcd9
Andy <andrewpeliza@hotmail.com>
2018-06-06 16:07:07 +0100

    HttpClient implemented, Interceptors implemented.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,5 @@
 import {Component, Injectable} from '@angular/core';
-import {Response} from '@angular/http';
+// import {HttpEvent, HttpEventType} from '@angular/common/http';
 
 import {DataStorageService} from '../../shared/data-storage.service';
 import {AuthService} from '../../auth/auth.service';
@@ -19,7 +19,7 @@ export class HeaderComponent {
   onSaveData() {
     this.dataStorageService.storeRecipes()
       .subscribe(
-        (response: Response) => {
+        (response) => {
           console.log(response);
         }
       );
@@ -34,8 +34,8 @@ export class HeaderComponent {
     this.router.navigate(['/signin']);
   }
 
-  isAuthenticated() {
-    return this.authService.isAuthenticated();
-  }
+  // isAuthenticated() {
+  //   return this.authService.isAuthenticated();
+  // }
 
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
-import 'rxjs/add/operator/map';
+import {HttpClient, HttpHeaders, HttpParams, HttpRequest} from '@angular/common/http';
+import 'rxjs/Rx';
 
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
@@ -8,24 +8,30 @@ import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http,
+  constructor(private http: HttpClient,
               private recipeService: RecipeService,
               private authService: AuthService) {
   }
 
   storeRecipes() {
-    const token = this.authService.getToken();
-
-    return this.http.put('https://ng-recipe-book-9f435.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    // return this.http.put('https://ng-recipe-book-9f435.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+    //   observe: 'body',
+    //   params: new HttpParams().set('auth', token)
+    //   // headers: new HttpHeaders().set()
+    // });
+    const req = new HttpRequest('PUT', 'https://ng-recipe-book-9f435.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {reportProgress: true});
+    return this.http.request(req);
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
+    // this.http.get<Recipe[]>('https://ng-recipe-book-9f435.firebaseio.com/recipes.json?auth=' + token)
 
-    this.http.get('https://ng-recipe-book-9f435.firebaseio.com/recipes.json?auth=' + token)
+    this.http.get<Recipe[]>('https://ng-recipe-book-9f435.firebaseio.com/recipes.json', {
+      observe: 'body',
+      responseType: 'json'
+    })
       .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        (recipes) => {
           for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               console.log(recipes);
